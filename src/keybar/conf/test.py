89c209e1e1cf307378fded10218bed3d119c492a
Christopher Grebs <cg@webshox.org>
2014-09-27 22:06:47 +0200

    Update testing configuration to allow certification verification

--- src/keybar/conf/test.py
+++ src/keybar/conf/test.py
@@ -1,1 +1,18 @@
 from keybar.conf.base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'keybar_test',
+    }
+}
+
+certificates_dir = os.path.join(PROJECT_DIR, 'extras', 'certificates')
+
+KEYBAR_SERVER_CERTIFICATE = os.path.join(certificates_dir, 'server.crt')
+KEYBAR_SERVER_KEY = os.path.join(certificates_dir, 'server.key')
+
+KEYBAR_CLIENT_CERTIFICATE = os.path.join(certificates_dir, 'ca.crt')
+KEYBAR_CLIENT_KEY = os.path.join(certificates_dir, 'ca.key')
+
+KEYBAR_CA_BUNDLE = os.path.join(certificates_dir, 'ca.db.certs', '01.pem')
