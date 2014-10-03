5d1c36a0dfef2da8c0e72bc30183e8f4931ad2a4
Christopher Grebs <cg@webshox.org>
2014-10-03 21:25:56 +0200

    Configure grunt-sass and grunt-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,44 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			options: {
+				includePaths: ['bower_components/foundation/scss'],
+			},
+			dist: {
+				options: {
+					outputStyle: "expanded"
+				},
+				files: {
+					"src/keybar/static/css/app.css": "src/keybar/static/scss/app.scss"
+				},
+			}
+		},
+		watch: {
+			options: {
+				livereload: {
+					key: grunt.file.read('extras/certificates/server.key'),
+					cert: grunt.file.read('extras/certificates/server.crt')
+				},
+			},
+			grunt: {
+				files: ["Gruntfile.js"]
+			},
+			sass: {
+				files: "src/keybar/static/scss/app.scss",
+				tasks: ["sass"],
+			},
+			html: {
+				files: ['src/keybar/templates/keybar/web/*.html']
+			}
+		}
 	});
 
 	grunt.loadTasks('extras/grunt/tasks');
+
+	grunt.loadNpmTasks("grunt-sass");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.registerTask("build", ["sass"]);
 
 	grunt.registerTask(
 		'default',
