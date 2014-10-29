659647b1c16a4520f7246d117543804628f630a5
Christopher Grebs <cg@webshox.org>
2014-10-29 10:30:30 +0100

    Use proper download_import_cldr script from babel

--- extras/import_cldr.sh
+++ extras/import_cldr.sh
@@ -2,4 +2,4 @@
 
 path=$(python -c "import os, babel; print(os.path.join(os.path.dirname(babel.__file__), os.pardir))")
 cd $path
-python setup.py import_cldr
+python scripts/download_import_cldr.py
