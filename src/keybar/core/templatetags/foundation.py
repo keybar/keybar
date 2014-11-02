a9cfcef9de081e3d85e6d16e651f306fd3c698ae
Christopher Grebs <cg@webshox.org>
2014-11-02 19:51:31 +0100

    Proper styling for login template

--- src/keybar/core/templatetags/foundation.py
+++ src/keybar/core/templatetags/foundation.py
@@ -14,7 +14,18 @@ ERROR_LEVEL_MAPPING = {
     message_constants.ERROR: 'alert'
 }
 
+PROVIDER_REPLACEMENTS = {
+    'google': 'google_plus',
+}
+
 
 @register.simple_tag
 def map_error_level(level):
     return mark_safe(ERROR_LEVEL_MAPPING[level])
+
+
+@register.simple_tag
+def provider_icon(provider):
+    if provider.id not in PROVIDER_REPLACEMENTS:
+        return provider.id
+    return PROVIDER_REPLACEMENTS[provider.id]
