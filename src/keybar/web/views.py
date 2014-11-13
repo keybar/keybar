f90b196b95425ddc273f45923387ae7e12299e78
Christopher Grebs <cg@webshox.org>
2014-11-13 02:43:08 +0100

    Remove unused views, now that we have django-allauth

--- src/keybar/web/views.py
+++ src/keybar/web/views.py
@@ -1,45 +1,6 @@
-from django.core.urlresolvers import reverse_lazy, reverse
-from django.contrib import auth
-from django.views.generic import TemplateView, FormView, RedirectView
-from django.views.generic.edit import CreateView
-from django.contrib import messages
-from django.utils.translation import ugettext_lazy as _
-
-from keybar.web.forms import RegisterForm, LoginForm
+from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     """View for the index page"""
     template_name = 'keybar/web/index.html'
-
-
-class RegisterView(CreateView):
-    template_name = 'keybar/web/register.html'
-    form_class = RegisterForm
-    success_url = reverse_lazy('keybar-index')
-
-    def form_valid(self, form):
-        response = super(RegisterView, self).form_valid(form)
-        messages.success(self.request, _('You registered successfully'))
-        return response
-
-
-class LoginView(FormView):
-    template_name = 'keybar/web/login.html'
-    form_class = LoginForm
-    success_url = reverse_lazy('keybar-index')
-
-    def form_valid(self, form):
-        response = super(LoginView, self).form_valid(form)
-        messages.success(self.request, _('You were successfully logged in'))
-        auth.login(self.request, form.get_user())
-        return response
-
-
-class LogoutView(RedirectView):
-    permanent = False
-
-    def get_redirect_url(self, *args, **kwargs):
-        auth.logout(self.request)
-        messages.success(self.request, _('You were successfully logged out'))
-        return reverse('keybar-index')
