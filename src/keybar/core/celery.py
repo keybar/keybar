28a2f566bd3459e49b008320c1cdcd8d1fd396c7
Christopher Grebs <cg@webshox.org>
2014-11-13 14:25:18 +0100

    formatting

--- src/keybar/core/celery.py
+++ src/keybar/core/celery.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "keybar.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'keybar.settings')
 
 from django.conf import settings
 
