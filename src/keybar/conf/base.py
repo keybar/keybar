7fe6723fb9e06ae6e3a87a01b5c248669a770c65
Christopher Grebs <cg@webshox.org>
2014-11-27 23:45:31 +0100

    Force usage of postgresql

--- src/keybar/conf/base.py
+++ src/keybar/conf/base.py
@@ -71,8 +71,8 @@ AUTH_USER_MODEL = 'keybar.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'keybar_dev',
     }
 }
 
