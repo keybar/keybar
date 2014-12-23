c8b71bddc1dfd730f6839b49cc60691fbaf22929
Christopher Grebs <cg@webshox.org>
2014-12-23 04:22:51 +0100

    Add compat UserManager to allow for (super-)user-creation via manage.py

--- src/keybar/models/user.py
+++ src/keybar/models/user.py
@@ -11,9 +11,32 @@ from django.db import models
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser, UserManager as BaseUserManager
 
 from keybar.core.tasks import send_mail_async
+
+
+class UserManager(BaseUserManager):
+    """Compatibility layer for our email-only api."""
+
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True, **extra_fields)
 
 
 class User(AbstractBaseUser):
