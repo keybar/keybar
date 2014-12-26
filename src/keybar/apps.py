644db713f78989fd79b5668adbe318289761d455
Christopher Grebs <cg@webshox.org>
2014-12-26 17:12:04 +0100

    Fix weird recursive side-effect in keybar.apps

--- src/keybar/apps.py
+++ src/keybar/apps.py
@@ -7,9 +7,6 @@ class KeybarConfig(AppConfig):
     verbose_name = _('Keybar')
 
     def ready(self):
-        # Just initialize everything… weird import paths
-        import keybar.wsgi  # noqa
-
         # We do like our application structure more than django's
         # so we import our modules manually.
         from keybar.models import user, entry, device  # noqa
