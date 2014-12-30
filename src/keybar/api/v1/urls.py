073f1e5a0a4cb1ca064b91a021a92d5534a1ec70
Christopher Grebs <cg@webshox.org>
2014-12-30 15:06:02 +0100

    Add drf-extensions and a small example so that I don't forget to use it

--- src/keybar/api/v1/urls.py
+++ src/keybar/api/v1/urls.py
@@ -1,9 +1,18 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url, patterns, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_extensions.routers import ExtendedActionLinkRouterMixin
 
 from keybar.api.v1 import users
 
 
-urlpatterns = patterns('',
+class Router(ExtendedActionLinkRouterMixin, DefaultRouter):
+    pass
+
+
+router = Router()
+router.register('users', users.UserViewSet)
+
+urlpatterns = [
     # Hookup our REST Api
-    url(r'^users/', users.ListView.as_view())
-)
+    url(r'^', include(router.urls)),
+]
--- src/keybar/api/v1/users.py
+++ src/keybar/api/v1/users.py
@@ -1,9 +1,17 @@
-from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework_extensions.routers import ExtendedSimpleRouter
+from rest_framework_extensions.decorators import link
 
 from keybar.models.user import User
 from keybar.serializers.user import UserSerializer
 
 
-class ListView(generics.ListAPIView):
+
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @link()
+    def test(self, request, pk=None):
+        return Response(['test'])
