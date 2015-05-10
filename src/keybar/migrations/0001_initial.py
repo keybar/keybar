6bc6ee52f1e07e861f212d09c60ce758805e5ff8
Christopher Grebs <cg@webshox.org>
2015-05-11 00:48:05 +0200

    Reset migrations

--- src/keybar/migrations/0001_initial.py
+++ src/keybar/migrations/0001_initial.py
@@ -2,12 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.contrib.postgres.fields
 import keybar.utils.db.json
 import uuid
+import django.utils.timezone
 import keybar.models.user
 from django.conf import settings
-import django.utils.timezone
-import django.contrib.postgres.fields
 
 
 class Migration(migrations.Migration):
@@ -20,15 +20,14 @@ class Migration(migrations.Migration):
             name='User',
             fields=[
                 ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, verbose_name='last login', null=True)),
-                ('id', models.UUIDField(primary_key=True, serialize=False, default=uuid.uuid4)),
-                ('email', models.EmailField(max_length=254, verbose_name='Email', unique=True)),
-                ('name', models.TextField(max_length=100, blank=True, verbose_name='Name', null=True)),
-                ('is_active', models.BooleanField(verbose_name='active', help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', default=True)),
-                ('date_joined', models.DateTimeField(verbose_name='date joined', default=django.utils.timezone.now)),
-                ('is_staff', models.BooleanField(verbose_name='staff status', help_text='Designates whether the user can log into this admin site.', default=False)),
-                ('is_superuser', models.BooleanField(verbose_name='superuser status', help_text='Designates that this user has all permissions without explicitly assigning them.', default=False)),
-                ('secret', models.BinaryField()),
+                ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('email', models.EmailField(max_length=254, unique=True, verbose_name='Email')),
+                ('name', models.TextField(max_length=100, null=True, verbose_name='Name', blank=True)),
+                ('is_active', models.BooleanField(default=True, verbose_name='active', help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('is_staff', models.BooleanField(default=False, verbose_name='staff status', help_text='Designates whether the user can log into this admin site.')),
+                ('is_superuser', models.BooleanField(default=False, verbose_name='superuser status', help_text='Designates that this user has all permissions without explicitly assigning them.')),
             ],
             options={
                 'verbose_name': 'User',
@@ -41,28 +40,144 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Device',
             fields=[
-                ('id', models.UUIDField(primary_key=True, serialize=False, default=uuid.uuid4)),
-                ('name', models.TextField(blank=True, verbose_name='Device name', default='')),
-                ('public_key', models.TextField(verbose_name='Device Public Key', unique=True)),
-                ('authorized', models.NullBooleanField(verbose_name='Authorized?', default=None)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='devices')),
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('name', models.TextField(default='', verbose_name='Device name', blank=True)),
+                ('public_key', models.BinaryField(unique=True, verbose_name='Device Public Key')),
+                ('authorized', models.NullBooleanField(default=None, verbose_name='Authorized?')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, blank=True, related_name='devices')),
             ],
+            options={
+                'abstract': False,
+            },
         ),
         migrations.CreateModel(
             name='Entry',
             fields=[
-                ('id', models.UUIDField(primary_key=True, serialize=False, default=uuid.uuid4)),
-                ('title', models.TextField(blank=True, verbose_name='Title', default='')),
-                ('url', models.URLField(blank=True, default='')),
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('title', models.TextField(default='', verbose_name='Title', blank=True)),
+                ('url', models.URLField(default='', blank=True)),
                 ('identifier', models.TextField(verbose_name='Identifier for login', help_text='Usually a username or email address')),
                 ('value', models.BinaryField(verbose_name='The encrypted value for the entry.', help_text='Usually a password.')),
-                ('description', models.TextField(blank=True, verbose_name='Description', default='')),
-                ('tags', django.contrib.postgres.fields.ArrayField(blank=True, size=None, base_field=models.TextField(blank=True), null=True)),
-                ('salt', models.BinaryField(blank=True, null=True)),
+                ('description', models.TextField(default='', verbose_name='Description', blank=True)),
+                ('tags', django.contrib.postgres.fields.ArrayField(size=None, base_field=models.TextField(blank=True), null=True, blank=True)),
+                ('salt', models.BinaryField(null=True, blank=True)),
                 ('keys', keybar.utils.db.json.JSONField(default={})),
                 ('log', keybar.utils.db.json.JSONField(default={})),
                 ('force_two_factor_authorization', models.BooleanField(default=False)),
-                ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='Organization',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=64)),
+                ('slug', models.SlugField(unique=True)),
+                ('date_added', models.DateTimeField(default=django.utils.timezone.now)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='OrganizationMember',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('email', models.EmailField(max_length=254, null=True, blank=True)),
+                ('type', models.CharField(choices=[('member', 'Member'), ('admin', 'Admin'), ('owner', 'Owner')], max_length=64, default='member')),
+                ('date_added', models.DateTimeField(default=django.utils.timezone.now)),
+                ('organization', models.ForeignKey(to='keybar.Organization')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='OrganizationMemberTeam',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('organization_member', models.ForeignKey(to='keybar.OrganizationMember')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Team',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('slug', models.SlugField()),
+                ('name', models.CharField(max_length=64)),
+                ('date_added', models.DateTimeField(default=django.utils.timezone.now)),
+                ('organization', models.ForeignKey(to='keybar.Organization')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='TeamVault',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('team', models.ForeignKey(to='keybar.Team')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='Vault',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('slug', models.SlugField(null=True)),
+                ('name', models.CharField(max_length=200)),
+                ('date_added', models.DateTimeField(default=django.utils.timezone.now)),
+                ('organization', models.ForeignKey(to='keybar.Organization')),
+                ('teams', models.ManyToManyField(to='keybar.Team', through='keybar.TeamVault', blank=True)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='teamvault',
+            name='vault',
+            field=models.ForeignKey(to='keybar.Vault'),
+        ),
+        migrations.AddField(
+            model_name='organizationmemberteam',
+            name='team',
+            field=models.ForeignKey(to='keybar.Team'),
+        ),
+        migrations.AddField(
+            model_name='organizationmember',
+            name='teams',
+            field=models.ManyToManyField(to='keybar.Team', through='keybar.OrganizationMemberTeam', blank=True),
+        ),
+        migrations.AddField(
+            model_name='organizationmember',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='organization_members'),
+        ),
+        migrations.AddField(
+            model_name='organization',
+            name='members',
+            field=models.ManyToManyField(to=settings.AUTH_USER_MODEL, related_name='organization_memberships', through='keybar.OrganizationMember'),
+        ),
+        migrations.AddField(
+            model_name='organization',
+            name='owner',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='entry',
+            name='vault',
+            field=models.ForeignKey(to='keybar.Vault'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='vault',
+            unique_together=set([('organization', 'slug')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='team',
+            unique_together=set([('organization', 'slug')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='organizationmemberteam',
+            unique_together=set([('team', 'organization_member')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='organizationmember',
+            unique_together=set([('organization', 'user'), ('organization', 'email')]),
         ),
     ]
