3dbe963ce1b36314a325f281bd4d4c5d8c2cb2d4
Christopher Grebs <cg@webshox.org>
2015-05-15 17:09:33 +0200

    Actually test that not associated devices are not listed on /devices/ endpoint.

--- src/keybar/tests/api/endpoints/test_devices.py
+++ src/keybar/tests/api/endpoints/test_devices.py
@@ -3,6 +3,7 @@ import mock
 
 from keybar.client import Client
 from keybar.models.device import Device
+from keybar.utils.crypto import generate_rsa_keys
 
 from keybar.tests.factories.device import (
     AuthorizedDeviceFactory, PUBLIC_KEY, PRIVATE_KEY)
@@ -34,6 +35,13 @@ class TestDevicesEndpoint:
     def test_list(self, settings, keybar_liveserver):
         user = UserFactory.create(is_superuser=True)
         device = AuthorizedDeviceFactory.create(user=user)
+
+        # Create a second device that is not owned by the user
+        # and should not show in the list.
+        AuthorizedDeviceFactory.create(
+            public_key=generate_rsa_keys()[1].exportKey('DER')
+        )
+
         client = Client(device.id, PRIVATE_KEY)
 
         endpoint = '{0}/api/devices/'.format(keybar_liveserver.url)
