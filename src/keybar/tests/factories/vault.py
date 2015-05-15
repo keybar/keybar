efc87e6ec8a4ddab7c3c5fbe9f06d0c6aa8089f4
Christopher Grebs <cg@webshox.org>
2015-05-15 03:29:19 +0200

    generate a name for a vault

--- src/keybar/tests/factories/vault.py
+++ src/keybar/tests/factories/vault.py
@@ -2,11 +2,14 @@
 import factory
 
 from keybar.models.vault import Vault
+from keybar.utils.test import get_random_name
 from keybar.tests.factories.organization import OrganizationFactory
 
 
 class VaultFactory(factory.DjangoModelFactory):
     organization = factory.SubFactory(OrganizationFactory)
+
+    name = factory.Sequence(lambda i: get_random_name(8, 32))
 
     class Meta:
         model = Vault
