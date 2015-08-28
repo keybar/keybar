a8ad6f15070dd6677bd9296174d0a9da396c2506
Christopher Grebs <cg@webshox.org>
2015-08-28 17:10:08 +0200

    Upgrade libraries

--- setup.py
+++ setup.py
@@ -14,15 +14,15 @@ def read(*parts):
 
 test_requires = [
     # General test libraries
-    'tox>=1.9.2,<2.0.0',
+    'tox>=2.1.1,<2.2.0',
     'pytest>=2.7.0,<2.8',
     'pytest-django>=2.8,<2.9',
     'pytest-isort==0.1.0',
 
     # Pep8 and code quality checkers
-    'pyflakes>=0.8.1,<0.9',
+    'pyflakes>=0.9.2,<0.10',
     'coverage>=3.7.1,<3.8',
-    'pytest-cov>=1.8.1,<1.9',
+    'pytest-cov>=2.1,<2.2',
     'pytest-flakes>=1.0,<1.1',
     'pytest-pep8>=1.0.6,<1.1',
     'pep8>=1.6.2,<1.7',
@@ -30,7 +30,7 @@ test_requires = [
 
     # Fixtures, test helpers
     'factory-boy>=2.5.1,<2.6',
-    'mock>=1.0.1,<1.1',
+    'mock>=1.3.0,<1.4',
     'hypothesis',
     'hypothesis-pytest',
 ]
@@ -42,12 +42,12 @@ install_requires = [
 
     # Configuration
     'django-configurations==0.8',
-    'django-cache-url==0.8.0',
+    'django-cache-url==1.0.0',
     'dj-database-url==0.3.0',
 
     # User login, sessions, social integration
-    'django_user_sessions>=1.1.1,<1.2.0',
-    'django-allauth>=0.22.0,<0.23',
+    'django_user_sessions>=1.2.0,<1.3.0',
+    'django-allauth>=0.23.0,<0.24',
 
     # For async worker support
     'celery>=3.1.18,<3.2',
@@ -58,25 +58,25 @@ install_requires = [
     'django-babel>=0.3.9',
 
     # For our REST Api
-    'djangorestframework>=3.1.3,<3.2',
+    'djangorestframework>=3.2.3,<3.3',
     'httpsig>=1.1.2,<1.2.0',
-    'requests>=2.6.0,<2.7',
+    'requests>=2.7,<2.8',
     'requests-toolbelt>=0.4,<0.5',
 
     # Form helpers
     'django-floppyforms>=1.5.1,<1.6',
 
     # For our development (and probably production?) tls pre-configured server.
-    'tornado>=4.1,<5.0',
+    'tornado>=4.2.1,<5.0',
 
     # All the crypto libs we ever need
-    'cryptography>=0.9.3,<1.0',
+    'cryptography>=1.0,<1.1',
     'certifi',
     'qrcode>=5.1,<5.2',
     'pycrypto>=2.6.1,<2.7',
 
     # Used to generate QR Codes
-    'Pillow>=2.8,<2.9',
+    'Pillow>=2.9,<3.0',
 
     # WSGI utilities
     'werkzeug>=0.9',
@@ -84,7 +84,7 @@ install_requires = [
     # For the client... resides here until we split all the code
     # to a separate repository. Keep it simple for now.
     'httpie>=0.9.2,<1.0',
-    'click>=4.0,<5.0',
+    'click>=5.1,<5.2',
 
     # for `generate_identities` script
     'py509>=0.0.5',
