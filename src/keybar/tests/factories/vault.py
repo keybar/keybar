8a51eead53011453ff337e3505c5ca2ccc18da66
Christopher Grebs <cg@webshox.org>
2016-02-28 14:57:20 +0100

    Remove usage of 'get_random_name' from vault factory

--- src/keybar/tests/factories/vault.py
+++ src/keybar/tests/factories/vault.py
@@ -1,15 +1,13 @@
-
 import factory
 
 from keybar.models.vault import Vault
 from keybar.tests.factories.user import UserFactory
-from keybar.utils.test import get_random_name
 
 
 class VaultFactory(factory.DjangoModelFactory):
     owner = factory.SubFactory(UserFactory)
 
-    name = factory.Sequence(lambda i: get_random_name(8, 32))
+    name = 'test vault'
 
     class Meta:
         model = Vault
