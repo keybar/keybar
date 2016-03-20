c0bd43d78ce0ca47dca6682b82dd3429f1872d73
Christopher Grebs <cg@webshox.org>
2016-03-20 09:37:21 +0100

    Don't allow empty Device.user anymore

--- src/keybar/models/device.py
+++ src/keybar/models/device.py
@@ -20,7 +20,7 @@ class Device(KeybarModel):
     each client is requested to get a initial device-id for the very
     first request.
     """
-    user = models.ForeignKey('keybar.User', related_name='devices', null=True, blank=True)
+    user = models.ForeignKey('keybar.User', related_name='devices')
     name = models.TextField(_('Device name'), blank=True, default='')
 
     public_key = models.TextField(_('Device Public Key'), unique=True)
