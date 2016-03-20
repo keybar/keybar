9ae0ccaaafec6770772587e604b207e7bea82103
Christopher Grebs <cg@webshox.org>
2016-03-20 15:25:51 +0100

    Simplify api parsing and remove not needed helpers

--- src/keybar/api/parsers.py
+++ src/keybar/api/parsers.py
@@ -1,27 +1,10 @@
 from django.conf import settings
-from rest_framework import renderers
-from rest_framework.parsers import BaseParser, ParseError
-
-from keybar.utils import json
+from rest_framework.parsers import JSONParser
 
 
-class JSONParser(BaseParser):
+class JSONParser(JSONParser):
+    """Parses JSON-serialized data.
+
+    This handles our custom media-type properly .
     """
-    Parses JSON-serialized data.
-    """
-
-    media_type = 'application/json'
-    renderer_class = renderers.JSONRenderer
-
-    def parse(self, stream, media_type=None, parser_context=None):
-        """
-        Parses the incoming bytestream as JSON and returns the resulting data.
-        """
-        parser_context = parser_context or {}
-        encoding = parser_context.get('encoding', settings.DEFAULT_CHARSET)
-
-        try:
-            data = stream.read().decode(encoding)
-            return json.loads(data)
-        except ValueError as exc:
-            raise ParseError('JSON parse error - {}%s'.format(exc))
+    media_type = 'application/vnd.keybar+json'
